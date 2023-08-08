b430911cce570fe70418bc0a76a65ec71f1c9481
chriscodehead <chrischikabox@gmail.com>
2023-08-08 17:50:23 +0100

    new update

---
+++ terms.php
@@ -0,0 +1,270 @@
+<?php
+require_once('library.php');
+$active2 = 'active';
+$title = 'About | We provide the first-class solar energy solutions to families and businesses across the - ' . $siteName;
+require_once('head.php'); ?>
+
+<body>
+ <div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+ <div class="preloader">
+  <div class="preloader-body">
+   <div class="cssload-container">
+    <div class="cssload-speeding-wheel"></div>
+   </div>
+  </div>
+ </div>
+ <div class="page">
+  <!-- Header-->
+  <?php require_once('header.php'); ?>
+  <!-- Breadcrumbs-->
+  <div class="breadcrumbs-custom bg-white">
+   <div class="container">
+    <ul class="breadcrumbs-custom-path">
+     <li><a href="./">Home</a></li>
+     <li><a>Pages</a></li>
+     <li class="active">Who we are</li>
+    </ul>
+   </div>
+  </div>
+  <section class="section section-md bg-image context-dark" style="background-image: url(images/about-01-1920x800.jpg)">
+   <div class="container">
+    <div class="row">
+     <div class="col-xl-9">
+      <h1>We Provide Innovative Solar Energy Solutions</h1>
+      <p class="small text-gray font-weight-medium">Our motto</p>
+      <h4 class="text-primary">“Making solar & alternative energy <br class="d-none d-sm-block"> affordable throughout the Globe”</h4>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <section class="section section-lg bg-light">
+   <div class="container">
+    <div class="row justify-content-between">
+     <div class="col-md-5 pb-5"><img src="img/team3.jpg" alt="" width="460" height="280" loading="lazy" />
+     </div>
+     <div class="col-lg-6">
+      <h2>About Us</h2>
+      <p>
+       Welcome to <?php print $siteName; ?> – Your Partner in Sustainable Energy Solutions!
+      </p>
+      <p>At <?php print $siteName; ?>, we are passionate about harnessing the power of renewable energy to create a greener and more sustainable future. As a leading solar and wind turbine installation contracting company, we specialize in providing innovative and eco-friendly energy solutions to residential, commercial, and industrial clients.</p>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <section class="section section-lg bg-white text-center">
+   <div class="container">
+    <div class="row justify-content-center">
+     <div class="col-xl-12 pb-5"><img src="img/team1.png" alt="" width="" height="" loading="lazy" /></div>
+     <div class="col-xl-9">
+      <h5>Our Mission & Vision</h5>
+      <p>Our vision is to be at the forefront of the renewable energy revolution, empowering communities with cleaner and more efficient energy alternatives. We aim to drive positive change by promoting sustainable practices and reducing our carbon footprint through the widespread adoption of solar and wind technologies.</p>
+      <p>At <?php print $siteName; ?>, our mission is to deliver cutting-edge, customized solar and wind energy solutions that cater to the unique needs of our clients. We strive to exceed customer expectations by offering top-notch services, using the latest technologies, and adhering to the highest industry standards.</p>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <div class="section-divider">
+   <div class="container">
+    <hr />
+   </div>
+  </div>
+
+
+
+  <section class="section section-lg bg-light">
+   <div class="container">
+    <div class="row justify-content-center text-center">
+     <div class="col-xl-8 col-md-10">
+      <h2>Our Difference <br class="d-none d-xl-block"> from Other Solar Power Companies</h2>
+      <p></p>
+     </div>
+    </div>
+    <div class="row row-30 offset-md">
+     <div class="col-lg-4 col-sm-6">
+      <article class="post-classic">
+       <figure class="post-classic-figure"><a class="link-img" href="#"><img src="img/photo9.jpg" alt="" width="360" height="240" loading="lazy" /></a>
+       </figure>
+       <div class="post-classic-body">
+        <h4 class="post-classic-title"><a href="#">Expertise and Experience</a></h4>
+        <p class="post-classic-text">
+         Our team of skilled engineers, technicians, and project managers has extensive experience in the design, installation, and maintenance of solar panel systems and wind turbines. We bring a wealth of knowledge to every project, ensuring optimal performance and long-term reliability.</p>
+       </div>
+      </article>
+     </div>
+     <div class="col-lg-4 col-sm-6">
+      <article class="post-classic">
+       <figure class="post-classic-figure"><a class="link-img" href="#"><img src="img/photo10.jpg" alt="" width="360" height="240" loading="lazy" /></a>
+       </figure>
+       <div class="post-classic-body">
+        <h4 class="post-classic-title"><a href="#">Customized Solutions</a></h4>
+        <p class="post-classic-text">
+         We understand that every energy project is unique. That's why we take the time to assess your specific requirements and develop tailor-made solutions that maximize energy efficiency and cost-effectiveness.</p>
+       </div>
+      </article>
+     </div>
+     <div class="col-lg-4 col-sm-6">
+      <article class="post-classic">
+       <figure class="post-classic-figure"><a class="link-img" href="#"><img src="img/photo18.jpg" alt="" width="360" height="240" loading="lazy" /></a>
+       </figure>
+       <div class="post-classic-body">
+        <h4 class="post-classic-title"><a href="#">Quality and Innovation</a></h4>
+        <p class="post-classic-text">
+         At <?php print $siteName; ?>, we are committed to delivering top-quality products and services. We partner with reputable manufacturers to source the best solar panels and wind turbines in the market, integrating the latest innovations and technologies into our installations. </p>
+       </div>
+      </article>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <section class="section section-lg bg-light text-center pb-0">
+   <div class="container">
+    <div class="row justify-content-center">
+     <div class="col-xl-8 col-md-10">
+      <h3>We're a passionate team working toward a better future for the planet.</h3>
+      <p></p><a class="btn btn-white" href="team">Our Team</a>
+     </div>
+    </div>
+   </div>
+   <div class="img-about-wrap"><img src="img/team2.jpg" alt="" width="1433" height="512" loading="lazy" />
+   </div>
+  </section>
+
+  <!-- Solar Plans that Fit Your Needs-->
+  <section class="section section-md bg-image" style="background-image: url(images/bg-img-02-1920x600.jpg)">
+   <div class="container">
+    <div class="row row-30">
+     <div class="col-md-7 wow fadeInLeft">
+      <h2>Solar Plans that Fit Your Needs</h2>
+     </div>
+     <div class="col-md-5 wow fadeInRight">
+      <p>Looking forward to power your home with solar energy? Call us and we'll help you find the right pricing plan that meets your needs.</p>
+      <div class="offset-40"><a class="btn btn-white" href="tel:<?php print $sitePhone; ?>"><?php print $sitePhone; ?></a></div>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <section style="display: none;" class="section section-lg bg-white text-center">
+   <div class="container">
+    <h5>Our Partners</h5>
+    <h2>We Work with 20+ Companies</h2>
+    <!-- Owl Carousel-->
+    <div class="owl-carousel offset-lg" data-items="2" data-sm-items="3" data-md-items="4" data-lg-items="5" data-xl-items="6" data-owl="{&quot;autoplayTimeout&quot;:3500}">
+     <div class="partner"><a href="#"><img src="images/partner-01-160x80.png" alt="" width="160" height="80" loading="lazy" /></a></div>
+     <div class="partner"><a href="#"><img src="images/partner-02-160x80.png" alt="" width="160" height="80" loading="lazy" /></a></div>
+     <div class="partner"><a href="#"><img src="images/partner-03-160x80.png" alt="" width="160" height="80" loading="lazy" /></a></div>
+     <div class="partner"><a href="#"><img src="images/partner-04-160x80.png" alt="" width="160" height="80" loading="lazy" /></a></div>
+     <div class="partner"><a href="#"><img src="images/partner-05-160x80.png" alt="" width="160" height="80" loading="lazy" /></a></div>
+     <div class="partner"><a href="#"><img src="images/partner-06-160x80.png" alt="" width="160" height="80" loading="lazy" /></a></div>
+    </div>
+   </div>
+  </section>
+
+  <section class="section section-lg bg-white pt-5">
+   <div class="container">
+    <div class="row justify-content-center">
+     <div class="col-xl-8 col-md-10">
+      <h3 class="text-center"><?php print $siteName; ?> has earned numerous awards since its establishment in 1999</h3>
+      <div class="row row-30 offset-md">
+       <div class="col-sm-6">
+        <ul class="list-marked list-lg">
+         <li>
+          <p><span class="font-weight-bold">2005</span> - Solar Power Award for collaboration with community</p>
+         </li>
+         <li>
+          <p><span class="font-weight-bold">2010</span> - Intelegant Award for Excellence (Boston Business Awards)</p>
+         </li>
+         <li>
+          <p><span class="font-weight-bold">2011, 2012, 2013</span> - Inc. 1000 List</p>
+         </li>
+         <li>
+          <p><span class="font-weight-bold">2014, 2015, 2016</span> - B Corp Certified</p>
+         </li>
+        </ul>
+       </div>
+       <div class="col-sm-6">
+        <ul class="list-marked list-lg">
+         <li>
+          <p><span class="font-weight-bold">2017</span> - Top 100 on Inc 1000 List</p>
+         </li>
+         <li>
+          <p><span class="font-weight-bold">2018</span> - Best Residential Solar Power Contractor (Solar & Storage Live Awards)</p>
+         </li>
+         <li>
+          <p><span class="font-weight-bold">2019, 2020</span> - Residential Solar & Storage Project of the Year</p>
+         </li>
+         <li>
+          <p><span class="font-weight-bold">2019, <?php print $siteYear; ?></span> - PV America Photovoltaic Project of Distinction Award (Sullivan's Ledge)</p>
+         </li>
+        </ul>
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <!-- Get in touch-->
+  <section class="section section-md section-get-in-touch bg-light">
+   <div class="container">
+    <div class="row row-30 justify-content-between">
+     <div class="col-xl-5 col-md-6 wow fadeInLeft">
+      <h2>Get in touch with our team today</h2>
+      <p>Contact our team of solar energy experts to have a custom solar energy solution installed for you.</p>
+      <div class="unit offset-md">
+       <div class="unit-left">
+        <div class="icon-circle bg-white">
+         <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="64" height="64" rx="32" fill="white" />
+          <path d="M34.6268 25.2049C35.6555 25.4052 36.601 25.9074 37.3421 26.6471C38.0832 27.3868 38.5864 28.3304 38.7871 29.3572M34.6268 21C36.7641 21.237 38.7572 22.1923 40.2787 23.709C41.8003 25.2257 42.7599 27.2138 43 29.3467M41.9468 37.7354V40.889C41.948 41.1818 41.8879 41.4716 41.7704 41.7398C41.6529 42.0081 41.4805 42.2489 41.2644 42.4468C41.0482 42.6447 40.793 42.7954 40.5152 42.8892C40.2373 42.983 39.9429 43.0178 39.6507 42.9915C36.4098 42.64 33.2966 41.5346 30.5613 39.7642C28.0166 38.1503 25.859 35.9968 24.242 33.4569C22.462 30.7145 21.3543 27.5922 21.0085 24.3429C20.9822 24.0522 21.0168 23.7592 21.1102 23.4826C21.2035 23.206 21.3536 22.9518 21.5507 22.7362C21.7479 22.5206 21.9878 22.3484 22.2554 22.2305C22.5229 22.1125 22.812 22.0515 23.1045 22.0512H26.2642C26.7753 22.0462 27.2708 22.2269 27.6584 22.5595C28.046 22.8922 28.2991 23.3541 28.3706 23.8593C28.504 24.8685 28.7513 25.8595 29.1079 26.8132C29.2496 27.1895 29.2803 27.5984 29.1963 27.9915C29.1123 28.3846 28.9171 28.7455 28.6339 29.0313L27.2963 30.3663C28.7957 32.9981 30.9789 35.1772 33.6157 36.6737L34.9533 35.3386C35.2397 35.056 35.6012 34.8612 35.9951 34.7774C36.389 34.6935 36.7987 34.7241 37.1756 34.8656C38.1312 35.2214 39.124 35.4683 40.1352 35.6014C40.6468 35.6734 41.1141 35.9307 41.4481 36.3241C41.7821 36.7176 41.9596 37.2198 41.9468 37.7354Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+         </svg>
+        </div>
+       </div>
+       <div class="unit-body">
+        <dl class="desc-list">
+         <dt class="desc-title">Phone number:</dt>
+         <dd class="desc-text h4"><a href="tel:<?php print $sitePhone; ?>"><?php print $sitePhone; ?></a></dd>
+        </dl>
+       </div>
+      </div>
+     </div>
+     <div class="col-md-6 wow fadeInRight">
+      <form class="form-box form-box-lg rd-mailform" data-form-output="form-output-global" data-form-type="contact" method="post" action="" enctype="multipart/form-data">
+       <div class="form-wrap">
+        <label class="form-label form-label-outside" for="contact-name">Your name</label>
+        <input class="form-input" id="name" type="text" name="name" data-constraints="@Required" placeholder="Name">
+       </div>
+       <div class="form-wrap">
+        <label class="form-label form-label-outside" for="contact-phone">Contact phone</label>
+        <input class="form-input" id="phone" type="text" name="phone" data-constraints="@Numeric" placeholder="(555) 555-5555">
+       </div>
+       <div class="form-wrap">
+        <label class="form-label form-label-outside" for="contact-email">Email address</label>
+        <input class="form-input" id="email" type="email" name="email" data-constraints="@Email @Required" placeholder="e.g. adam.anderson@demolink.org">
+       </div>
+       <div class="form-wrap">
+        <label class="form-label form-label-outside" for="contact-message">Anything else to add?</label>
+        <textarea class="form-input" id="message" name="message" data-constraints="@Required" placeholder="Your message"></textarea>
+       </div>
+       <div class="form-wrap">
+        <div class="custom-control">
+         <input class="custom-control-input" id="check-1" type="checkbox">
+         <label class="custom-control-label" for="check-1">Yes, I want to stay up to date on the latest in solar! <br class="d-none d-xl-block"> </label>
+        </div>
+       </div>
+       <div class="form-wrap">
+        <button class="btn btn-block btn-dark" type="button" id="n" onClick="contatMail();">Submit Message</button>
+       </div>
+      </form>
+     </div>
+    </div>
+   </div>
+  </section>
+
+  <!-- Footer-->
+  <?php require_once('footer.php'); ?>
